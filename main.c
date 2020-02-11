4c76198c34efdf237b9e46f53647c6c9552b507c
felipends <dasilvajfelipe@gmail.com>
2020-02-10 22:50:43 -0300

    populating alphabet

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//return the size of encoded information in bytes
+size_t lzw_encoder(char** info, int size);
+
 int main(int argc, char** argv) {
     FILE* input_fp;
     char* buffer;
@@ -18,7 +21,41 @@ int main(int argc, char** argv) {
 
     fclose(input_fp);
 
-    printf("%s\n", buffer);
+    lzw_encoder(&buffer, fsize);
+
+    return 0;
+}
+
+size_t lzw_encoder(char** info, int size) {
+    char* alphabet = NULL;
+    int alphabet_size = 0;
+
+    //flag
+    short hasSymbol = 0;
+    
+    for(int i = 0; i < size; i++) {
+        if (alphabet == NULL) {
+            alphabet = malloc(1);
+            alphabet[alphabet_size] = (*info)[i];
+            alphabet_size++;
+        } else {
+            for(int alpha_i = 0; alpha_i < alphabet_size; alpha_i++) {
+                if(alphabet[alpha_i] == (*info)[i]){
+                    hasSymbol = 1;
+                    break;
+                }
+            }
+            if(!hasSymbol){
+                alphabet = realloc(alphabet, alphabet_size+1);
+                alphabet[alphabet_size] = (*info)[i];
+                alphabet_size++;
+            }
+            
+            hasSymbol = 0;
+        }
+    }
+
+    printf("%s\n", alphabet);
 
     return 0;
 }
